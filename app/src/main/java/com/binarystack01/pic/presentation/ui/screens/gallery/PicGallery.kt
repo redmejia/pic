4ed5c558dd726ccdcab94bcff085bc44a4c31e3a
redmejia <reynaldoemejia@gmail.com>
2024-10-17 23:10:16 -0700

    directory structure and initial dependencies

---
+++ app/src/main/java/com/binarystack01/pic/presentation/ui/screens/gallery/PicGallery.kt
@@ -0,0 +1,16 @@
+package com.binarystack01.pic.presentation.ui.screens.gallery
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PicGallery() {
+    Text(text = "My Gallery")
+}
+
+@Preview
+@Composable
+fun PicGalleryPreview(){
+    PicGallery()
+}
