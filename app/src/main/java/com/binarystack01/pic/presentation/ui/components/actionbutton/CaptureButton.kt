783f5e5a690bf4ecb0328fb2647c79d81b3def86
redmejia <reynaldoemejia@gmail.com>
2024-10-22 10:39:20 -0700

    action capture button

---
+++ app/src/main/java/com/binarystack01/pic/presentation/ui/components/actionbutton/CaptureButton.kt
@@ -0,0 +1,79 @@
+package com.binarystack01.pic.presentation.ui.components.actionbutton
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+
+
+@Composable
+fun CaptureButton(
+    onClick: () -> Unit = {},
+    clicked: MutableState<Boolean>,
+) {
+
+    val scale by animateFloatAsState(
+        targetValue = if (!clicked.value) 1.2f else 1f,
+        animationSpec = tween(durationMillis = 300, easing = FastOutSlowInEasing),
+        label = "alpha"
+    )
+
+    Box(
+        modifier = Modifier
+            .scale(scale)
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        // outer circle
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .border(width = 3.dp, color = Color(0xFFD9D9D9), shape = CircleShape)
+                .background(color = Color.Black, CircleShape)
+        )
+        // inner circle
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .background(color = Color(0xFFD9D9D9), shape = CircleShape)
+        )
+    }
+
+    LaunchedEffect(clicked.value) {
+        if (clicked.value) {
+            delay(300)
+            clicked.value = false
+        }
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun CaptureButtonPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+//            .systemBarsPadding(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+//        CaptureButton()
+    }
+}
